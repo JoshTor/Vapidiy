c34004b1066b3a500c84dd21417bd83449bc7345
Joshua Torri <j.torri@kaiman.org>
2018-10-10 13:53:37 +0200

    Reprise version 0.2

--- vapidy/src/app/app.component.ts
+++ vapidy/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { BasePage } from "../pages/base/base";
 
 @Component({
   templateUrl: 'app.html'
@@ -21,8 +22,8 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'Calculer un e-liquide', component: HomePage },
+      { title: 'Calculer une base', component: BasePage }
     ];
 
   }
--- vapidy/src/pages/home/home.ts
+++ vapidy/src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController  } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,51 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    parome: number;
+    totaLiquide: number;
 
-  }
+    gouttes: number;
+    gouttesML: number;
+
+    erreur: boolean = false;
+    strResultat: string[] = ["", "", ""];
+
+    constructor(public navCtrl: NavController, public toastCtrl: ToastController) {
+
+    }
+
+    public precisionRound(nb: number,precision: number) {
+    var factor = Math.pow(10, precision);
+    return Math.round(nb * factor) / factor;
+    }
+
+    private showGouttes()
+    {
+      if(this.parome < 50 && this.parome > 1 && this.totaLiquide < 1000 && this.totaLiquide > 1 )
+      {
+        var qArome = this.parome/100*this.totaLiquide;
+        var nbGouttes = qArome*30;
+        var liquideRestant = this.totaLiquide - qArome;
+        this.strResultat[0] = "Résultat de votre composition finale : ";
+        this.strResultat[1] = "Arôme : "+this.precisionRound(qArome, 1)+" mL soit "+this.precisionRound(nbGouttes, 0)+" gouttes. ";
+        this.strResultat[2] = "Liquide : "+this.precisionRound(liquideRestant, 1)+" mL";
+        this.erreur = false;
+      }
+      else
+      {
+        this.strResultat[0] = "";
+        this.strResultat[1] = "";
+        this.strResultat[2] = "";
+        let toastError = this.toastCtrl.create({
+          message: "Des champs sont manquants ou incorrects",
+          duration: 2000,
+          position: 'top'
+        });
+        toastError.present();
+        this.erreur = true;
+      }
+
+
+    }
 
 }
